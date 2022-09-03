117665c4b6b18032bbe6ee3e181d59dd05f2ef48
Mirwais Farahi <waisfarahi@gmail.com>
2022-09-03 20:07:06 +0500

    react app file

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  
+
+  state = {advice: ''};
+
+  render() {
+    return (
+      <div className="App">
+        <h1>App</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
